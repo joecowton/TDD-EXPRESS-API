f9def7d6bc73d9245fdeb389aaa12eb246f95736
Joe Cowton <joecowton@gmail.com>
2018-02-06 10:11:27 +0000

    resolves es6 issue, creates server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import bodyParser from 'body-parser'
+import methodOverride from 'method-override';
+
+const app = express();
+import config from './config/config';
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+mongoose.connect(config.db);
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to' + config.db);
+});
+
+app.listen(config.port, (err) => {
+  if(err) throw err;
+  console.log('App listening on port' + config.port);
+})
