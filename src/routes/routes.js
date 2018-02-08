c3cd88d98ac2003303946c84047bbd78d0aa3372
Joe Cowton <joecowton@gmail.com>
2018-02-08 11:49:27 +0000

    creates users routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,4 +8,10 @@ module.exports = (app) => {
   app.get('/api/records/:id', RecordController.find)
   app.delete('/api/records/:id', RecordController.delete)
   app.put('/api/records/:id', RecordController.update)
+
+  app.get('/api/users', RecordController.show)
+  app.post('/api/users', RecordController.create)
+  app.get('/api/users/:id', RecordController.find)
+  app.delete('/api/users/:id', RecordController.delete)
+  app.put('/api/users/:id', RecordController.update)
 }
